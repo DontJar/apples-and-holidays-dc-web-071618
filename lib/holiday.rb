ebc00b3e8beeb118fb028ffa2e1ffe65a3c940f9
Jon Schulman <schulman99@gmail.com>
2018-06-27 22:26:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |event, supply|
-      supply.include?("BBQ")
+      prints event if supply.include?("BBQ")
     end
   end
 end
