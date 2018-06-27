4e7c7e83d8bd248f902b52860e9d0d9b103bc902
Jon Schulman <schulman99@gmail.com>
2018-06-27 22:21:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,7 +33,7 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.each do |season, holiday|
+  holiday_hash.select do |season, holiday|
     holiday.map do |event, supply|
       puts "#{event} has a BBQ!" if supply == "BBQ"
     end
